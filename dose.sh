2d2adf96d378bf7d5b72255bac129b09c984a187
Heinz-Wilhelm Schäbe <pepsclena@gmx.de>
2016-06-11 14:31:07 +0200

    Grundgerüst
    Mal schauen :-)

---
+++ dose.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Ein bidiirektorales Dosentelefon auf Basis von VLC auf dem RPi
+
+#TODO: machen
+
+#Initralisieren
+# Gegenstelle erreichbar?
+
+
+#Klingeln -> später
+
+# GPIO Taster -> später
+
+# vlc streamt audio über LAN - paralell: VLC empfängt über LAN (Komandos mit & verbinden)
+
+#Verbindung wird irgendwann beendet
+
+
+# Programende oder while-schleife XXX?
