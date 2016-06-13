b68e2d4af98fd590d97780ad5d11e2ff81fbbb43
hwshws <hwshws@users.noreply.github.com>
2016-06-13 12:06:22 +0200

    Update dose.sh

--- dose.sh
+++ dose.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cvlc -vvv alsa://hw:1,0 --sout '#transcode{acode=vorb,ab=128}:standard{access=http,mux=ogg,url=10.0.15.75:8080,cashing=500}'& sleep 5; while true; do; sleep1; mplayer http://10.0.15.76:8080; done
